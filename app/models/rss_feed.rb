448f9ce6b04e1cf0093bc01301495c39b2cfb38c
James Balazs <james.balazs@outlook.com>
2024-06-09 19:12:01 +0100

    add group model

--- app/models/rss_feed.rb
+++ app/models/rss_feed.rb
@@ -2,3 +2,4 @@ class RssFeed < ApplicationRecord
   validates_presence_of :title, :url
   validates_length_of :title, maximum: 32
 end
+bundle exec rails g scaffold Article title:string body:text sentiment:float
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_03_170602) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_09_180935) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "rss_feeds", force: :cascade do |t|
     t.string "title", null: false
