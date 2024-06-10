0e7b7c2fb48f46ca0076360db6e887199a2265c7
James Balazs <james.balazs@outlook.com>
2024-06-10 22:01:19 +0100

    remove excess articles routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles, only: [:index] do
+    post :fetch
+  end
   resources :rss_feeds, except: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -9,40 +9,4 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     get articles_url
     assert_response :success
   end
-
-  test "should get new" do
-    get new_article_url
-    assert_response :success
-  end
-
-  test "should create article" do
-    assert_difference("Article.count") do
-      post articles_url, params: { article: { Group_id: @article.Group_id, RssFeed_id: @article.RssFeed_id, body: @article.body, sentiment: @article.sentiment, title: @article.title } }
-    end
-
-    assert_redirected_to article_url(Article.last)
-  end
-
-  test "should show article" do
-    get article_url(@article)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_article_url(@article)
-    assert_response :success
-  end
-
-  test "should update article" do
-    patch article_url(@article), params: { article: { Group_id: @article.Group_id, RssFeed_id: @article.RssFeed_id, body: @article.body, sentiment: @article.sentiment, title: @article.title } }
-    assert_redirected_to article_url(@article)
-  end
-
-  test "should destroy article" do
-    assert_difference("Article.count", -1) do
-      delete article_url(@article)
-    end
-
-    assert_redirected_to articles_url
-  end
 end
